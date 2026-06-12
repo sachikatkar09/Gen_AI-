c0e5b66affd20bdc4a4646a9bd5b76cf94a9b96c
sachikatkar09 <sachikatkar09@gmail.com>
2026-06-13 03:39:28 +0530

    Implement user authentication with register and login endpoints

--- src/app.js
+++ src/app.js
@@ -14,11 +14,11 @@ app.use(express.json());
 // );
 
 /* require all the routes here */
-// const authRouter = require("./routes/auth.routes");
+const authRouter = require("./routes/auth.routes");
 // const interviewRouter = require("./routes/interview.routes");
 
 /* using all the routes here */
-// app.use("/api/auth", authRouter);
+app.use("/api/auth", authRouter);
 // app.use("/api/interview", interviewRouter);
 
 module.exports = app;
