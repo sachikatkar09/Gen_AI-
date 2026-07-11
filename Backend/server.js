3f2e2b40ae70b82a3043957c0332ec95e8101bc6
sachikatkar09 <sachikatkar09@gmail.com>
2026-07-11 14:16:11 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,16 @@ const connectToDB = require("./src/config/database");
 
 connectToDB();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
